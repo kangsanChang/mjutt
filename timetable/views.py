7cdcc0f34e3983364ee1315f1b335a3dd9a24f58
kangsanChang <rkdtks1005@gmail.com>
2017-04-17 14:07:24 +0900

    working on index.html

--- timetable/views.py
+++ timetable/views.py
@@ -5,7 +5,13 @@ from django.db.models import Q
 from .models import Classitem
 from .forms import ClassitemForm
 from django.http import HttpResponse
+from .switcher import switch_to_deptname
+
 
 def index(request):
-    items = Classitem.objects.all()
+    if request.method == 'POST':
+        # render filtered table
+        print("aaa")
+    else:
+        items = Classitem.objects.all()
     return render(request, "timetable/index.html", {"items" : items})
