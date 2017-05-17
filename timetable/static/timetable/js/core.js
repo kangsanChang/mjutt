a166cccd6c00a127d1851dfbfc1114c2b10c1859
kangsanChang <rkdtks1005@gmail.com>
2017-05-17 13:00:29 +0900

    working on js, add js linter

--- timetable/static/timetable/js/core.js
+++ timetable/static/timetable/js/core.js
@@ -5,7 +5,7 @@ function check_insert(){
     alert("close");
   }
 }
-
+// day는 classtime에서 파싱해서 가져온 한글을 code로 바꿔줌
 function day_to_code(day){
   switch (day){
     case '월' : ret = 'Mon'; break;
@@ -17,17 +17,17 @@ function day_to_code(day){
   return ret;
 }
 
-function random_table_color(){
-  var colors = new Array("red","orange","yellow","olive","green","teal","blue",
-  "violet","purple","pink","grey","black");
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function random_item_color(){
-  var colors = new Array();
-  // selected라고 되어있는 element 찾아서 배열에서 중복되는 색 뺴준 후 랜덤 리턴.
-
-}
+// function random_table_color(){
+//   var colors = new Array("red","orange","yellow","olive","green","teal","blue",
+//   "violet","purple","pink","grey","black");
+//   return colors[Math.floor(Math.random() * colors.length)];
+// }
+//
+// function random_item_color(){
+//   var colors = [];
+//   // selected라고 되어있는 element 찾아서 배열에서 중복되는 색 뺴준 후 랜덤 리턴.
+//
+// }
 
 function time_parser(elem){
   var row_dict = {};
@@ -35,7 +35,7 @@ function time_parser(elem){
   classname = $(elem)[0].children[1].innerHTML;
   prof = $(elem)[0].children[4].innerHTML;
   classtime = $(elem)[0].children[7].innerHTML.split(","); //["월13:00-14:50 (Y5420)", "수13:00-14:50 (Y5420)"]
-  classcode = $(elem)[0].children[5].innerHTML;
+  classcode = $(elem)[0].children[5].innerText; // innerHTML은 trim이 안됨
   daypattern = /(월|화|수|목|금)/;
   timepattern = /\d{2}:\d{2}/g;
   classroompattern = /\w\d{3,5}/g;
@@ -56,7 +56,7 @@ function time_parser(elem){
       classroom = value.match(classroompattern);
       days.push(day[0]); // push() method : new value to array, append()는 html에 추가할때 사용, day가 array라서 index붙임
       times.push(time);
-      if(classroom == null){
+      if(classroom === null){
         classrooms.push("empty");
       }else{
         classrooms.push(classroom[0]);
@@ -64,102 +64,100 @@ function time_parser(elem){
     }
   });
 
-  row_dict['classname'] = classname;
-  row_dict['prof'] = prof;
-  row_dict['classtime'] = classtime;
-  row_dict['days'] = days;
-  row_dict['times'] = times;
-  row_dict['classrooms'] = classrooms;
-  row_dict['classcode'] = classcode;
-// Object {classname: "C언어프로그래밍", prof: "주우석", classtime: Array(2), days: Array(2), times: Array(2)…}
-// classname : "C언어프로그래밍"
-// classrooms : Array(2)
-// 0 : "Y5445"
-// 1 : "Y5445"
-// length : 2
-// classtime : Array(2)
-// 0 : "월15:00-16:50 (Y5445)"
-// 1 : "수15:00-16:50 (Y5445)"
-// length : 2
-// days : Array(2)
-// 0 : "월"
-// 1 : "수"
-// length : 2
-// prof : "주우석"
-// times : Array(2)
-// 0 : Array(2)
-  // 0 : "15:00"
-  // 1 : "16:50"
-  // length : 2
-// 1 : Array(2)
-  // 0 : "15:00"
-  // 1 : "16:50"
-  // length : 2
-// length : 2
+  // dict 접근법 2가지 dict['abc'] , dict.abc (linter에서는 dot notation을 쓰라함)
+
+  row_dict.classname = classname;
+  row_dict.prof = prof;
+  row_dict.classtime = classtime;
+  row_dict.days = days;
+  row_dict.times = times;
+  row_dict.classrooms = classrooms;
+  row_dict.classcode = classcode;
 
   return row_dict;
 }
-
-class CClassItem {
-  constructor(item){
-    this.item = item;
-    this.classname = item['classname'];
-    this.prof = item['prof'];
-    this.classtime = item['classtime']; // array
-    this.classcode = item['classcode'];
-  }
-
-  get item_id(){
-    return "#"+this.classcode;
-  }
-
-}
-
-class CClassElement extends CClassItem {
-  constructor(item, i){
-    this.day = item['days'][i];
-    this.shour = item["times"][i][0].split(":")[0]; // start hour
-    this.smin = item["times"][i][0].split(":")[1]; // start min
-    this.ehour = item["times"][i][1].split(":")[0]; // end hour
-    this.emin = item["times"][i][1].split(":")[1]; // end min
-    this.interval_minute = (parseInt(this.ehour) - parseInt(this.shour))*60 + (parseInt(this.emin) - parseInt(this.smin));
-    this.elem_top = "";
-    this.elem_left = "";
-    this.elem_width = "";
-    this.elem_height = "";
-  }
-
-  set position(){
-
-  }
-}
-
+//
+// class ClassItem {
+//   constructor(item){
+//     this.item = item;
+//     this.classname = item.classname;
+//     this.prof = item.prof;
+//     this.classtime = item.classtime; // array
+//     this.classcode = item.classcode;
+//   }
+//
+//   get item_id(){
+//     return "#" + this.classcode;
+//   }
+//
+// }
+//
+// class ClassElement extends ClassItem {
+//   constructor(item, i){
+//     super();
+//     this.day = item.days[i];
+//     this.shour = item.times[i][0].split(":")[0]; // start hour
+//     this.smin = item.times[i][0].split(":")[1]; // start min
+//     this.ehour = item.times[i][1].split(":")[0]; // end hour
+//     this.emin = item.times[i][1].split(":")[1]; // end min
+//     this.interval_minute = (parseInt(this.ehour) - parseInt(this.shour))*60 + (parseInt(this.emin) - parseInt(this.smin));
+//     this.elem_top = "";
+//     this.elem_left = "";
+//     this.elem_width = "";
+//     this.elem_height = "";
+//   }
+//
+//   update_position(){
+//     this.elem_width = $("."+this.shour).filter("."+this.day).width();  // cell 한개 너비가 곧 elem의 너비
+//     cell_height = $("."+this.shour).filter("."+this.day).height();  // cell 한 개 높이
+//     this.elem_height = Math.round(cell_height * (this.interval_minute/60));
+//
+//     // top position정하기, start hour와 min에 영향을 받는다
+//     if(this.smin=="00"){
+//       this.elem_top = $("."+this.shour).filter("."+this.day).offset().top +1;
+//     }else{
+//       // smin이 있을때 top 위치 내려와야 함
+//       this.elem_top = $("."+this.shour).filter("."+this.day).offset().top +1;
+//       smin_height = Math.round(cell_height *(this.smin/60));
+//       item_top += smin_height;
+//       // 10:30 시작이면 10시보다 한시간 cell의 1/2 높이만큼 멀어짐(top++)
+//     }
+//     this.elem_left = $("."+this.shour).filter("."+this.day).offset().left + 2;
+//     return;
+//   }
+// }
 
 function ClassItem(item){
-  this.classname = item['classname'];
-  this.prof = item['prof'];
-  this.classtime = item['classtime']; // array
-  this.classcode = item['classcode'];
+  this.classname = item.classname;
+  this.prof = item.prof;
+  this.classtime = item.classtime; // array
+  this.classcode = item.classcode;
 
   this.toString = function(){
     return classname + " / " + prof + " / " + classcode;
-  }
+  };
 
   this.getItemID = function(){
     return "#"+this.classcode;
-  }
+  };
 }
 
-function ClassElement(item){
+function ClassElement(item, i){
   // 한 엘리먼트 값임! 한 강의엔 여러 엘리먼트(일반적으로2개)가 있음
   // Constructor
 
   // property
-  this.day = item['days'][i];
-  this.shour = item["times"][i][0].split(":")[0]; // start hour
-  this.smin = item["times"][i][0].split(":")[1]; // start min
-  this.ehour = item["times"][i][1].split(":")[0]; // end hour
-  this.emin = item["times"][i][1].split(":")[1]; // end min
+  this.classname = item.classname;
+  this.prof = item.prof;
+  this.classtime = item.classtime; // array
+  this.classcode = item.classcode;
+  this.classroom = item.classrooms[i];
+
+  this.day = day_to_code(item.days[i]); // 월 -> Mon
+  this.shour = item.times[i][0].split(":")[0]; // start hour
+  this.smin = item.times[i][0].split(":")[1]; // start min
+  this.ehour = item.times[i][1].split(":")[0]; // end hour
+  this.emin = item.times[i][1].split(":")[1]; // end min
   this.interval_minute = (parseInt(this.ehour) - parseInt(this.shour))*60 + (parseInt(this.emin) - parseInt(this.smin));
   this.elem_top = "";
   this.elem_left = "";
@@ -168,9 +166,16 @@ function ClassElement(item){
 }
 
 // inheritance
-ClassElement.prototype = new ClassItem();
+// ClassElement.prototype = new ClassItem();
 
 // method
+ClassELement.prototype.makeItemHtml= function(){
+  html = '<div class="classitem" id='+this.classcode+'><div class="content">\
+  <p>'+this.classname+'<br>'+this.prof+'<br>'+this.classroom+'<br></p></div></div>';
+
+  return html;
+};
+
 ClassElement.prototype.setPosition = function(){
   this.elem_width = $("."+this.shour).filter("."+this.day).width();  // cell 한개 너비가 곧 elem의 너비
   cell_height = $("."+this.shour).filter("."+this.day).height();  // cell 한 개 높이
@@ -186,70 +191,71 @@ ClassElement.prototype.setPosition = function(){
     item_top += smin_height;
     // 10:30 시작이면 10시보다 한시간 cell의 1/2 높이만큼 멀어짐(top++)
   }
-  this.elem_left = $("."+this.shour).filter("."+this.day).offset().left + 2;
-}
+  this.elem_left = $("."+this.shour).filter("."+this.day).offset().left+ 2;
+};
 
 ClassElement.prototype.highlight_classitem = function(){
-  if(elem_height == ""){
+  if(this.elem_height === ""){
     console.log("element 의 위치가 설정되지 않았습니다.");
     return;
+  }else{
+    div = makeItemHtml();
   }
 
-}
+};
 
 ClassElement.prototype.hover_classitem = function(){
 
-}
-
-var classitems = new Array();
-
-// 시간 개수 만큼 element 생성해야 함
-$.each(item.times, function(i, val){
-  // val = item["times"][i]
-  item_id = "#" + item['classcode'];
-  var tag = '<div class"classitem" id="'+item_id+'"><div class="inner_content">\
-  <p>'+item["classname"]+'<br>'+item["prof"]+'<br>'+  '(강의실)' +'</p></div></div>';
-  get_element_detail(item, i);
-});
-
-function get_element_detail(item, i){
-  // 한 강의의 i번째 수업의 시간을 파싱해서 highlight에 필요한 정보 리턴
-  day = item['days'][i];
-  shour = item["times"][i][0].split(":")[0]; // start hour
-  smin = item["times"][i][0].split(":")[1]; // start min
-  ehour = item["times"][i][1].split(":")[0]; // end hour
-  emin = item["times"][i][1].split(":")[1]; // end min
-
-  element['interval_minute'] = (parseInt(ehour) - parseInt(shour))*60 + (parseInt(emin) - parseInt(smin));
-  element['cls_shour'] = "."+ shour;
-  element['cls_day'] = '.'+day;
-  element['smin'] = smin;  // top위치 정할 때 smin 필요함
-
-  return element;
-}
-
-function set_highlight(item_id, elem){
-  // 랜덤컬러도 해줘야 함
-  cell_width = $(elem['cls_shour']).filter(elem['cls_day']).width();
-  cell_height = $(elem['cls_shour']).filter(elem['cls_day']).height();
-
-  item_width = cell_width;
-  item_height = Math.round(cell_height * (elem['interval_minute']/60));
-
-  if(smin=="00"){
-    item_top = $(elem['cls_shour']).filter(elem['cls_day']).offset().top +1;
-  }else{
-    // smin이 있을때 top 위치 내려와야 함
-    item_top = $(elem['cls_shour']).filter(elem['cls_day']).offset().top +1;
-    smin_height = Math.round(cell_height *(element['smin']/60));
-
-    item_top += smin_height;
-  }
-
-  item_left = $(elem['cls_shour']).filter(elem['cls_day']).offset().left + 2;
-  $(item_id+".classitem").width(item_width).height(item_height).offset({top:item_top ,left:item_left});
-}
-
+};
+//
+// var classitems = [];
+//
+// // // 시간 개수 만큼 element 생성해야 함
+// // $.each(item.times, function(i, val){
+// //   // val = item.times[i]
+// //   item_id = "#" + item.classcode;
+// //   var tag = '<div class"classitem" id="'+item_id+'"><div class="inner_content">\
+// //   <p>'+item.classname+'<br>'+item.prof+'<br>'+  '(강의실)' +'</p></div></div>';
+// //   get_element_detail(item, i);
+// // });
+//
+// function get_element_detail(item, i){
+//   // 한 강의의 i번째 수업의 시간을 파싱해서 highlight에 필요한 정보 리턴
+//   day = item.days[i];
+//   shour = item.times[i][0].split(":")[0]; // start hour
+//   smin = item.times[i][0].split(":")[1]; // start min
+//   ehour = item.times[i][1].split(":")[0]; // end hour
+//   emin = item.times[i][1].split(":")[1]; // end min
+//
+//   element.interval_minute = (parseInt(ehour) - parseInt(shour))*60 + (parseInt(emin) - parseInt(smin));
+//   element.cls_shour = "."+ shour;
+//   element.cls_day = '.'+day;
+//   element.smin = smin;  // top위치 정할 때 smin 필요함
+//
+//   return element;
+// }
+//
+// function set_highlight(item_id, elem){
+//   // 랜덤컬러도 해줘야 함
+//   cell_width = $(elem.cls_shour).filter(elem.cls_day).width();
+//   cell_height = $(elem.cls_shour).filter(elem.cls_day).height();
+//
+//   item_width = cell_width;
+//   item_height = Math.round(cell_height * (elem.interval_minute/60));
+//
+//   if(smin=="00"){
+//     item_top = $(elem.cls_shour).filter(elem.cls_day).offset().top +1;
+//   }else{
+//     // smin이 있을때 top 위치 내려와야 함
+//     item_top = $(elem.cls_shour).filter(elem.cls_day).offset().top +1;
+//     smin_height = Math.round(cell_height *(elem.smin/60));
+//
+//     item_top += smin_height;
+//   }
+//
+//   item_left = $(elem.cls_shour).filter(elem.cls_day).offset().left + 2;
+//   $(item_id+".classitem").width(item_width).height(item_height).offset({top:item_top ,left:item_left});
+// }
 
 // function set_highlight(rowobj, option){
 //   // row의 time parsing 된 dictionary 받아 hover event 줌
@@ -321,8 +327,11 @@ function set_highlight(item_id, elem){
 // }
 
 function insert_classitem(elem){
+  console.log(elem);
   row = time_parser(elem);
-  set_highlight(row, "selected");
+  // set_highlight(row, "selected");
+  console.log(row);
+
   //remove hover
   elem.removeClass("hover");
   $("td").removeClass("hover");
@@ -332,7 +341,7 @@ function insert_classitem(elem){
 function activateCSS(){
   $('#table-body .table-row').hover(function(){
     $(this).addClass("hover");
-    set_highlight(time_parser($(this)),"hover");
+    // set_highlight(time_parser($(this)),"hover");
   }, function(){
     $(this).removeClass("hover");
     $("td").removeClass("hover");
@@ -355,97 +364,97 @@ function activateCSS(){
   });
 }
 
+//
+// function add_timetable(){
+//   //tab 개수 구한 후 추가
+//   switch ($('#timetable-tab').children('a').length) {
+//     case 1:
+//       data_tab = "second";
+//       tab_html = "PLAN B";
+//       color = random_table_color();
+//       break;
+//     case 2:
+//       data_tab = "third";
+//       tab_html = "PLAN C";
+//       color = random_table_color();
+//       break;
+//     default:
+//       alert("더이상 추가할 수 없습니다.");
+//       return;
+//   }
+//   $("#timetable-tab").children($('a')).removeClass("active"); // tab 의 a 태그 에서 active 제거
+//   $("#timetable-tab").siblings(".active").removeClass("active"); // tab 내용에서 active 제거
+//   $('<a class="item active" data-tab="'+data_tab+'">'+tab_html+'</a>').insertBefore($("#add-timetable"));
+//   $("#my-timetable").append($('<div class="ui bottom attached tab segment active" data-tab="'+data_tab+'">\
+//     <table class="ui celled '+color+' table">\
+//         <thead>\
+//           <tr>\
+//             <th>/</th>\
+//             <th>월요일</th>\
+//             <th>화요일</th>\
+//             <th>수요일</th>\
+//             <th>목요일</th>\
+//             <th>금요일</th>\
+//           </tr>\
+//         </thead>\
+//         <tbody>'));
+//   tbody_html="";
+//   for(i=1; i<13; i++){
+//     hour = i+8;
+//     if(hour.toString().length == 1){ // 9 시 이하면(한자리수 시간) 0 붙여줌
+//       start="0"+hour;
+//     }else{
+//       start=hour;
+//     }
+//     end = hour+1;
+//     tbody_html +=
+//     '<tr>\
+//       <td rowspan="2">'+i+'교시<br>('+start+'~'+end+')</td>\
+//       <td id="'+start+'-under30-Mon"></td>\
+//       <td id="'+start+'-under30-Tue"></td>\
+//       <td id="'+start+'-under30-Wed"></td>\
+//       <td id="'+start+'-under30-Thu"></td>\
+//       <td id="'+start+'-under30-Fri"></td>\
+//     </tr>\
+//     <tr>\
+//       <td id="'+start+'-over30-Mon"></td>\
+//       <td id="'+start+'-over30-Tue"></td>\
+//       <td id="'+start+'-over30-Wed"></td>\
+//       <td id="'+start+'-over30-Thu"></td>\
+//       <td id="'+start+'-over30-Fri"></td>\
+//     </tr>\
+//     ';
+//   }
+//   $("[data-tab="+data_tab+"]").children($("tbody")).append(tbody_html).append("</tbody>\
+//     </table>\
+//   </div>");
+//
+//   $('.tabular.menu .item').tab();  // Activate tab in semantic ui
+// }
 
-function add_timetable(){
-  //tab 개수 구한 후 추가
-  switch ($('#timetable-tab').children('a').length) {
-    case 1:
-      data_tab = "second";
-      tab_html = "PLAN B";
-      color = random_table_color();
-      break;
-    case 2:
-      data_tab = "third";
-      tab_html = "PLAN C";
-      color = random_table_color();
-      break;
-    default:
-      alert("더이상 추가할 수 없습니다.");
-      return;
-  }
-  $("#timetable-tab").children($('a')).removeClass("active"); // tab 의 a 태그 에서 active 제거
-  $("#timetable-tab").siblings(".active").removeClass("active"); // tab 내용에서 active 제거
-  $('<a class="item active" data-tab="'+data_tab+'">'+tab_html+'</a>').insertBefore($("#add-timetable"));
-  $("#my-timetable").append($('<div class="ui bottom attached tab segment active" data-tab="'+data_tab+'">\
-    <table class="ui celled '+color+' table">\
-        <thead>\
-          <tr>\
-            <th>/</th>\
-            <th>월요일</th>\
-            <th>화요일</th>\
-            <th>수요일</th>\
-            <th>목요일</th>\
-            <th>금요일</th>\
-          </tr>\
-        </thead>\
-        <tbody>'));
-  tbody_html="";
-  for(i=1; i<13; i++){
-    hour = i+8;
-    if(hour.toString().length == 1){ // 9 시 이하면(한자리수 시간) 0 붙여줌
-      start="0"+hour;
-    }else{
-      start=hour;
-    }
-    end = hour+1;
-    tbody_html +=
-    '<tr>\
-      <td rowspan="2">'+i+'교시<br>('+start+'~'+end+')</td>\
-      <td id="'+start+'-under30-Mon"></td>\
-      <td id="'+start+'-under30-Tue"></td>\
-      <td id="'+start+'-under30-Wed"></td>\
-      <td id="'+start+'-under30-Thu"></td>\
-      <td id="'+start+'-under30-Fri"></td>\
-    </tr>\
-    <tr>\
-      <td id="'+start+'-over30-Mon"></td>\
-      <td id="'+start+'-over30-Tue"></td>\
-      <td id="'+start+'-over30-Wed"></td>\
-      <td id="'+start+'-over30-Thu"></td>\
-      <td id="'+start+'-over30-Fri"></td>\
-    </tr>\
-    ';
-  }
-  $("[data-tab="+data_tab+"]").children($("tbody")).append(tbody_html).append("</tbody>\
-    </table>\
-  </div>");
-
-  $('.tabular.menu .item').tab();  // Activate tab in semantic ui
-}
-
-
-function remove_timetable(){
-  if($("#timetable-tab").children(".active").attr("data-tab") == "first"){
-    alert("PLAN A는 삭제할 수 없습니다.");
-    return;
-  }else{
-    $("#timetable-tab").children(".active").remove();
-    $("#timetable-tab").siblings(".active").remove();
-
-    $.each($("#timetable-tab").children('a'),function(index, value){
-      if(index==0){
-        $("#timetable-tab").children('a').eq(index).attr("data-tab","first");
-        $("#timetable-tab").siblings().eq(index).attr("data-tab","first");
-        $("#timetable-tab").children('a').eq(index).text("PLAN A");
-      }
-      if(index==1){
-        $("#timetable-tab").children('a').eq(index).attr("data-tab","second");
-        $("#timetable-tab").siblings().eq(index).attr("data-tab","second");
-        $("#timetable-tab").children('a').eq(index).text("PLAN B");
-      }
-    });
-    $("#timetable-tab").children('a').eq(0).addClass("active");
-    $("#timetable-tab").siblings().eq(0).addClass("active");
-  }
-  $('.tabular.menu .item').tab();  // Activate tab in semantic ui
-}
+//
+// function remove_timetable(){
+//   if($("#timetable-tab").children(".active").attr("data-tab") == "first"){
+//     alert("PLAN A는 삭제할 수 없습니다.");
+//     return;
+//   }else{
+//     $("#timetable-tab").children(".active").remove();
+//     $("#timetable-tab").siblings(".active").remove();
+//
+//     $.each($("#timetable-tab").children('a'),function(index, value){
+//       if(index===0){
+//         $("#timetable-tab").children('a').eq(index).attr("data-tab","first");
+//         $("#timetable-tab").siblings().eq(index).attr("data-tab","first");
+//         $("#timetable-tab").children('a').eq(index).text("PLAN A");
+//       }
+//       if(index==1){
+//         $("#timetable-tab").children('a').eq(index).attr("data-tab","second");
+//         $("#timetable-tab").siblings().eq(index).attr("data-tab","second");
+//         $("#timetable-tab").children('a').eq(index).text("PLAN B");
+//       }
+//     });
+//     $("#timetable-tab").children('a').eq(0).addClass("active");
+//     $("#timetable-tab").siblings().eq(0).addClass("active");
+//   }
+//   $('.tabular.menu .item').tab();  // Activate tab in semantic ui
+// }
